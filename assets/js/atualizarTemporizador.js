883a67c3300eaa02d408760317e2dfbe8f96818c
Washington Note Dell NW <washingtonlopes2003@gmail.com>
2024-11-28 09:48:17 -0300

    Upload repository

---
+++ assets/js/atualizarTemporizador.js
@@ -0,0 +1,21 @@
+function atualizarTemporizador() {
+    let check = document.getElementById('check').checked
+    const boxNumbers = document.getElementById('boxNumbers')
+    const boxProgressBar = document.querySelector('.box-progress-bar')
+
+    if (check) {
+        boxNumbers.style.display = 'flex'
+        boxProgressBar.style.display = 'block'
+        clearInterval(tempoTemporizador)
+        tempoTemporizador = setInterval(animacao(), capturarInputs())
+    } else {
+        boxNumbers.style.display = 'none'
+        boxProgressBar.style.display = 'none'
+        clearInterval(tempoTemporizador)
+        animating = false
+    }
+}
+
+document.getElementById('check').addEventListener('click', atualizarTemporizador)
+document.getElementById('inputMinutes').addEventListener('blur', atualizarTemporizador)
+document.getElementById('inputSeconds').addEventListener('blur', atualizarTemporizador)
